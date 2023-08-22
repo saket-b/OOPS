59ea6ccb91b9d4be0c45d44171b90b6cf7906bc2
saket-b <saketbharti201721@gmail.com>
2023-08-22 15:28:53 +0530

    added one file

--- template.cpp
+++ template.cpp
@@ -1,19 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-template <typename T>
+// template <typename T>
 
-T my_max(T x, T y)
+// T my_max(T x, T y)
+// {
+//     return max(x, y);
+// }
+
+// int main()
+// {
+
+//     cout<< my_max(3, 6)<<endl;
+//     cout<<my_max(4.5, 5.5)<<endl;
+//     cout<<my_max('d', 'g')<<endl;
+
+
+// }
+
+class student
 {
-    return max(x, y);
-}
-
+    public:
+    int marks;
+    public: student(){}
+    student(int x)
+    { 
+         marks=x; 
+    }
+};
 int main()
 {
+    student s1(100);
+    student s2();
+   student s3=100;
 
-    cout<< my_max(3, 6)<<endl;
-    cout<<my_max(4.5, 5.5)<<endl;
-    cout<<my_max('d', 'g')<<endl;
+    cout<<"saket"<<endl;
+    cout<<s3.marks<<endl;
 
-
+    return 0;
 }
